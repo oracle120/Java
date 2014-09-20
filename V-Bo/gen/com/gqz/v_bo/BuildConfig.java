2e5bf106c1a55451d1fc36773916997266ec346a
茉莉gsy <jinhuer168@163.com>
2014-09-20 23:03:34 +0800

    V-Bo客户端开发第一课
    Logo开发，渐变效果、全屏等

---
+++ V-Bo/gen/com/gqz/v_bo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gqz.v_bo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ V-Bo/src/com/gqz/v_bo/ui/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.gqz.v_bo.ui;
+
+import com.gqz.v_bo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		this.setContentView(R.layout.activity_main);
+	}
+
+}
