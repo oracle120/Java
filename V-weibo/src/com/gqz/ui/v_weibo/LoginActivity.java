b4cbe5dc92b68e4a72d882a0f2f3a1ae71889282
茉莉gsy <jinhuer168@163.com>
2014-09-20 22:37:35 +0800

    V-weibo客户端开发第一课
    Logo的开发，实现Logo及渐变、全屏效果

---
+++ V-weibo/src/com/gqz/ui/v_weibo/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.gqz.ui.v_weibo;
+
+import com.gqz.v_weibo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		this.setContentView(R.layout.activity_main);
+	}
+
+}
